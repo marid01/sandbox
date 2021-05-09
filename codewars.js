bc5bc8a1210fabf4a9a2efe510cbdd542448fb3a
Marid <maridshikhkerimov@gmail.com>
2021-05-10 00:29:03 +0300

    ajax

---
+++ codewars.js
@@ -0,0 +1,39 @@
+// let name = 'Sam Harris';
+//
+// function abbrevName(name) {
+//     let abb = name.split(' ').map(i => i.charAt(0));
+//     console.log(abb);
+//     let arr = abb.join('. ').toUpperCase();
+//     console.log(arr)
+//
+//
+//
+// }
+//
+//
+// abbrevName(name);
+//
+
+
+//---------------------------------------
+
+// let name = 'Ryan'
+//
+// function greet(name){
+//     return (`Hello, ${name} how are you doing today?`)
+// }
+//
+// console.log(greet(name))
+
+//---------------------------------------
+
+
+
+const areaOrPerimeter = function(l , w) {
+    let b = 2 * (l + w);
+    let a = b.toFixed(0);
+    return a;
+};
+console.log(areaOrPerimeter(4.55, 4))
+
+// console.log(areaOrPerimeter());
---
+++ server.js
@@ -0,0 +1,19 @@
+//JSON формат передачи данных, глубокое копирования
+'use strict';
+
+// const persone = {
+//     name: 'Alex',
+//     tel: '+744444444',
+//     parents: {
+//         mon: 'Olga',
+//         dad: 'Mike',
+//     }
+// };
+//
+// const clone = JSON.parse(JSON.stringify(persone));
+// clone.parents.mon = 'Ann';
+// console.log(persone);
+// console.log(clone);
+
+//AJAX
+
