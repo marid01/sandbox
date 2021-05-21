93035a33d79e03c64777229252b1b47822e68c80
Marid <maridshikhkerimov@gmail.com>
2021-05-21 18:13:20 +0300

    codewars

--- codewars.js
+++ codewars.js
@@ -29,11 +29,24 @@
 
 
 
-const areaOrPerimeter = function(l , w) {
-    let b = 2 * (l + w);
-    let a = b.toFixed(0);
-    return a;
-};
-console.log(areaOrPerimeter(4.55, 4))
+// const areaOrPerimeter = function(l , w) {
+//     let b = 2 * (l + w);
+//     let a = b.toFixed(0);
+//     return a;
+// };
+// console.log(areaOrPerimeter(4.55, 4))
 
 // console.log(areaOrPerimeter());
+
+
+
+function generateRange(min, max, step) {
+    const result = [];
+    for (let i = min; i <= max; i += step) {
+        result.push(i)
+    }
+    return result
+}
+
+
+console.log(generateRange(2, 10, 2));
